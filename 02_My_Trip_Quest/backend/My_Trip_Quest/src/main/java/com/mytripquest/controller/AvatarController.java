48cc237a718305031953a76b4465ba486813f6e6
차지훈 <zoas1564@naver.com>
2025-12-04 15:34:42 +0900

    fix: 아바타 컨트롤러 URL 패턴 수정

--- 02_My_Trip_Quest/backend/My_Trip_Quest/src/main/java/com/mytripquest/controller/AvatarController.java
+++ 02_My_Trip_Quest/backend/My_Trip_Quest/src/main/java/com/mytripquest/controller/AvatarController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/avatar")
+@RequestMapping("/api/v1/avatar")
 @RequiredArgsConstructor
 public class AvatarController {
 
