02fa2c91e1a4331b773af28c0a3cb07e1e25d962
JeongGwanWoo <jkw4805@naver.com>
2025-12-09 15:50:34 +0900

    feat: questmap 인증 연동 및 userId 제거

--- 02_My_Trip_Quest/frontend/src/api/index.js
+++ 02_My_Trip_Quest/frontend/src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useAuthStore } from '@/stores/auth';
 
 const api = axios.create({
   baseURL: 'http://localhost:8080', 
@@ -10,6 +11,11 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
+    const authStore = useAuthStore();
+    const token = authStore.token;
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => {
