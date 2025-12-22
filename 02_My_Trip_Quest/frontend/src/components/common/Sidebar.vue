5b7ae16f98f4e51381dc147e32f9f5115e0fe56b
JeongGwanWoo <jkw4805@naver.com>
2025-12-22 17:57:53 +0900

    feat:여행 페이지 추가
    관광지, 시군구, 분류 별로 검색 가능

--- 02_My_Trip_Quest/frontend/src/components/common/Sidebar.vue
+++ 02_My_Trip_Quest/frontend/src/components/common/Sidebar.vue
@@ -40,6 +40,16 @@
           <span class="text">퀘스트</span>
         </router-link>
 
+        <router-link to="/travel" class="nav-item">
+          <span class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 20px; height: 20px;">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+            </svg>
+          </span>
+          <span class="text">여행</span>
+        </router-link>
+
         <router-link to="/rankings" class="nav-item">
           <span class="icon">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 20px; height: 20px;">
